c2134f831825d047f4030173b3b041f5bea05497
hyensukim <crazymin17@naver.com>
2023-10-08 21:57:02 +0900

    chapter01

---
+++ 예제 소스/src/example/Start.java
@@ -0,0 +1,7 @@
+package example;
+
+public class Start{
+    public static void main(String[] args){
+        System.out.println("Hello, OOP!!");
+    }
+}
---
+++ 예제 소스/src/example/Start3.java
@@ -0,0 +1,18 @@
+package example;
+
+public class Start3 {
+    public static void main(String[] args) {
+        int i = 10;
+        int k = 20;
+
+        if(i==10){
+            int m = k+5;
+            k = m;
+        }
+        else{
+            int p = k +10;
+            k = p;
+        }
+        System.out.println(k);
+    }
+}
